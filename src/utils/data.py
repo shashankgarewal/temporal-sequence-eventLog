6bef6a6b44cc4b886147cceb55ca9e11f11bb9b7
shashankgarewal <s.garewal@alumni.iitg.ac.in>
2026-03-17 12:41:41 +0530

    Add compare 2 df func & update feature-profile

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from collections import defaultdict
-from typing import Dict, List, Any, Tuple
+from typing import Dict, List, Any
 
 
-def tag_feature_map(profile: Dict[str, Any]) -> Dict[str, List[str]]:
+def tag_feature_map(profile: Dict[str, Any], key = "columns") -> Dict[str, List[str]]:
     """
         Creates a tag → columns lookup from the column schema profile.
         
@@ -14,7 +14,7 @@ def tag_feature_map(profile: Dict[str, Any]) -> Dict[str, List[str]]:
             A dictionary with tag are keys and values are lists of columns.
         """
     tag_map = defaultdict(list)
-    columns = profile.get("columns", {})
+    columns = profile.get(key, {})
     for col, meta in columns.items():
         for tag in meta.get("tags", []):
             tag_map[tag].append(col)
@@ -35,3 +35,22 @@ def add_suffix(cols: List[str] | str, suffix: str = "", trim:int = 0) -> List[st
     if trim == 0:
         return [f"{col}{suffix}" for col in cols]
     return [f"{col[:-trim]}{suffix}" for col in cols]
+
+def compare_dfs(df1, df2):
+    """Diagnose whether 2 dataframes are same or not"""
+    if df1.shape != df2.shape:
+        print("shape mismatch")
+        return False
+    
+    if (set(df1.columns) ^ set(df2.columns)) != set():
+        print("different column names")
+        return False
+    
+    if (df1.dtypes != df2.dtypes).sum() != 0:
+        print("columns dtypes mismatch")
+        return False
+    
+    df1 = df1.sort_index().sort_index(axis=1)
+    df2 = df2.sort_index().sort_index(axis=1)
+    df1.compare(df2)
+    return df1.equals(df2)
